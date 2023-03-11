52273b0206e464f8653d3e7b54a7b8d0fe2b23e3
alexsandrozdr <andynhozdr@gmail.com>
2023-03-11 20:48:58 -0300

    container creation

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./components/pages/Home";
 import Company from "./components/pages/Company";
 import Contact from "./components/pages/Contact";
 import NewProject from "./components/pages/NewProject";
+import Container from "./components/layout/Container";
 
 function App() {
   return (
@@ -13,12 +14,14 @@ function App() {
         <Link to="/contact">Contact</Link>
         <Link to="/newproject">New Project</Link>
       </div>
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/company" element={<Company />} />
-        <Route exact path="/contact" element={<Contact />} />
-        <Route exact path="/newproject" element={<NewProject />} />
-      </Routes>
+      <Container customClass="min-height">
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/company" element={<Company />} />
+          <Route exact path="/contact" element={<Contact />} />
+          <Route exact path="/newproject" element={<NewProject />} />
+        </Routes>
+      </Container>
       <p>Footer</p>
     </Router>
   );
